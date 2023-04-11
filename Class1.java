c4d7b5d1798eea8fbfe0798315477ddccd45a7e5
Kholid Wirautama <108299019+KKovalskia@users.noreply.github.com>
2023-04-11 08:38:45 +0700

    Add files via upload

---
+++ Class1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.p4;
+import java.util.Scanner;
+
+/**
+ *
+ * @author C-18
+ */
+public class Class1 {
+      public static void main(String[] args) {
+        
+          Scanner input = new Scanner(System.in);
+          System.out.println("Masukkan variabel 1: ");
+          int variabel1 = input.nextInt();
+          System.out.println("Masukkan variabel 2: ");
+          int variabel2 = input.nextInt();         
+          System.out.println(" ");          
+          int variabel3 = (variabel1 + variabel2);
+          System.out.println("Variabel1 + Variabel2 = " +variabel3);
+          
+          System.out.println("Dikerjakan oleh Muhammad Kholid Wirauatma + 22201177");
+    }
+    
+}
